e249ff5d022ea5ce930c07f95f05c0ed79316954
Omar Kassem <omarnehadelkassemsayed.abelwahab@st.uskudar.edu.tr>
2022-12-19 18:33:12 +0300

    Hiding secret key

--- backend/core/beemedicals/settings.py
+++ backend/core/beemedicals/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0rhh2g8@%7se^l-d1pym_v&7reqbytm^0xk+^!a-hud1o*u4j('
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
